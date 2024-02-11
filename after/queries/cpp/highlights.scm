85d2129aa746054a7467dabbec7464feb699f564
NivekNK <kevin.rojas.v@mail.pucv.cl>
2024-02-11 10:03:53 -0300

    Testing color for struct

--- after/queries/cpp/highlights.scm
+++ after/queries/cpp/highlights.scm
@@ -1,5 +1,4 @@
 ; extends
-(struct_specifier
-  (type_identifier) @local.definition.var
-    (#set! "definition.var.scope" "global")) @type.struct
+((struct_specifier) @local.definition.var
+  (#set! "definition.var.scope" "global")) @type.struct
     (#set! "priority" 120)
