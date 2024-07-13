790ae79fb030c84ba615516a70498d87fbd24874
NivekNK <kevin.rojas.v@mail.pucv.cl>
2024-07-13 13:35:44 -0400

    change in colors

--- lua/nk-theme/colors.lua
+++ lua/nk-theme/colors.lua
@@ -1,6 +1,69 @@
-local util = require("nk-theme.colors_util")
+local hsl = require("nk-theme.colors_util").hsl_to_hex
 
 local M = {}
+
+---@class Palette
+local palette = {
+    none = "NONE",
+
+    red500 = "#c53b53",
+
+    magenta200 = hsl(344.82, 97.65, 83.33),
+    magenta300 = hsl(344.13, 89.63, 73.53),
+    magenta400 = hsl(341.34, 83.67, 61.57),
+    magenta500 = hsl(338.15, 81.59, 53.14),
+    magenta600 = hsl(338.57, 65.42, 41.96),
+    magenta700 = hsl(338.9, 57.96, 30.78),
+    magante800 = hsl(340, 48, 19.61),
+
+    cyan200 = hsl(195, 73.68, 77.65),
+    cyan300 = hsl(194.23, 65.99, 71.18),
+    cyan400 = hsl(193.58, 73.26, 63.33),
+    cyan500 = hsl(191.28, 89.34, 47.84),
+    cyan600 = hsl(191.34, 80.39, 40),
+    cyan700 = hsl(192.1, 73.91, 31.57),
+    cyan800 = hsl(192.15, 67.52, 22.94),
+
+    green200 = hsl(103.45, 28.16, 59.61),
+    green300 = hsl(95.6, 43.96, 59.41),
+    green400 = hsl(87.69, 60.19, 57.65),
+    green500 = hsl(80, 75.63, 53.33),
+    green600 = hsl(90.74, 56.81, 41.76),
+    green700 = hsl(99.51, 50, 32.16),
+    green800 = hsl(107, 46.15, 25.49),
+
+    orange200 = hsl(22.22, 71.43, 62.94),
+    orange300 = hsl(23.7, 81.82, 61.18),
+    orange400 = hsl(24.95, 90.48, 58.82),
+    orange500 = hsl(30.12, 100, 50),
+    orange600 = hsl(24.78, 80.72, 48.82),
+    orange700 = hsl(20.74, 71.68, 44.31),
+    orange800 = hsl(15.14, 61.33, 35.49),
+
+    gray200 = hsl(210, 20, 98.04),
+    gray300 = hsl(220, 14.29, 95.88),
+    gray400 = hsl(220, 13.04, 90.98),
+    gray500 = hsl(216, 12.2, 83.92),
+    gray600 = hsl(217.89, 10.61, 64.9),
+    gray700 = hsl(220, 8.94, 46.08),
+    gray800 = hsl(215, 13.79, 34.12),
+
+    purple200 = hsl(267.06, 73.91, 86.47),
+    purple300 = hsl(264.75, 81.63, 80.78),
+    purple400 = hsl(262.2, 84.75, 76.86),
+    purple500 = hsl(261.03, 86.67, 73.53),
+    purple600 = hsl(259.15, 45.63, 59.61),
+    purple700 = hsl(256.8, 32.19, 45.69),
+    purple800 = hsl(254.74, 34.55, 32.35),
+
+    yellow200 = hsl(67.16, 54.47, 75.88),
+    yellow300 = hsl(67.5, 56.34, 72.16),
+    yellow400 = hsl(67.83, 57.5, 68.63),
+    yellow500 = hsl(69.14, 59.32, 65.29),
+    yellow600 = hsl(66.1, 53.15, 56.47),
+    yellow700 = hsl(64.84, 51.67, 52.94),
+    yellow800 = hsl(63.26, 51.39, 49.22),
+}
 
 ---@return Palette
 function M.palette()
--- lua/nk-theme/colors_util.lua
+++ lua/nk-theme/colors_util.lua
@@ -1,49 +1,18 @@
 local M = {}
 
----@type string
-M.bg = nil
-
----@type string
-M.fg = nil
-
----@param c string Color in hex format
-local function hexToRgb(c)
-    c = string.lower(c)
-    return { tonumber(c:sub(2, 3), 16), tonumber(c:sub(4, 5), 16), tonumber(c:sub(6, 7), 16) }
-end
-
----@param foreground string foreground color
----@param background string background color
----@param alpha number|string number between 0 and 1. 0 results in bg, 1 results in fg
----@return string
-function M.blend(foreground, background, alpha)
-    alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
-
-    local bg = hexToRgb(background)
-    local fg = hexToRgb(foreground)
-
-    local blendChannel = function(i)
-        local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
-        return math.floor(math.min(math.max(0, ret), 255) + 0.5)
+---@param hue number
+---@param saturation number
+---@param lightness number
+---@return string Color in hexadecimal
+function M.hsl_to_hex(hue, saturation, lightness)
+    lightness = lightness / 100
+    local a = saturation * math.min(lightness, 1 - lightness) / 100
+    local f = function(n)
+        local k = (n + hue / 30) % 12
+        local color = lightness - a * math.max(math.min(k - 3, 9 - k, 1), -1)
+        return string.format("%02x", math.floor(255 * color + 0.5))
     end
-
-    return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
-end
-
----@param hex string color
----@param amount number number between 0 and 1. 0 results in bg, 1 results in fg
----@param bg string|nil background color
----@return string
-function M.darken(hex, amount, bg)
-    return M.blend(hex, bg or M.bg, amount)
-end
-
----@param hex string color
----@param amount number number between 0 and 1. 0 results in bg, 1 results in fg
----@param fg string|nil foreground color
----@return string
-function M.lighten(hex, amount, fg)
-    return M.blend(hex, fg or M.fg, amount)
+    return "#" .. f(0) .. f(8) .. f(4)
 end
 
 return M
